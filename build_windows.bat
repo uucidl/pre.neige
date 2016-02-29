d235d3da0f1e185329e49ecdf571c9a3e8f77000
Nicolas Léveillé <Nicolas Lveill>
2016-03-01 00:01:30 +0100

    Allow specifying optimization

--- build_windows.bat
+++ build_windows.bat
@@ -1,3 +1,4 @@
+@set CommonCLFlags=-O2
 @set MicrosDir=uu.micros
 @set MicrosCLFlags=-I%MicrosDir%/include/ -I%MicrosDir%/libs/glew/include/
 @set MicrosCLFlags=%MicrosDir%/libs/glew/src/glew.c -DGLEW_STATIC opengl32.lib %MicrosCLFlags%
@@ -9,4 +10,4 @@ mkdir output
 cl -c -Fo:output\hid.obj -I %HIDAPIDir%\hidapi %HIDAPIDir%\windows\hid.c
 @set HIDAPIProducts=output\hid.obj Setupapi.lib
 @set CLWarnings= -W4 -wd4312 -wd4100 -wd4459 -wd4838 -wd4456 -wd4068 -wd4457 -wd4244 -wd4305 -wd4267 -wd4996
-cl -Fo:output\ -Fe:neige neige.cpp %MicrosCLFlags% %HIDAPIProducts% -nologo -EHsc -FC -DOS=OS_WINDOWS -DCPU=CPU_IA64 -DNEIGE_SLOW %CLWarnings%
+cl %CommonCLFlags% -Fo:output\ -Fe:neige neige.cpp %MicrosCLFlags% %HIDAPIProducts% -nologo -EHsc -FC -DOS=OS_WINDOWS -DCPU=CPU_IA64 -DNEIGE_SLOW %CLWarnings%
