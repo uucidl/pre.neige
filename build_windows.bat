e6710082edd79d74d1ae400ff1a24685c77dfc7e
nicolas <nicolas@uucidl.com>
2016-03-20 13:39:01 +0100

    Extract vine effect from main unit
    This is to allow to work in isolation on the effect in a convenient
    manner.
    
    We rename neige.cpp -> neige_unit.cpp
    We define the source code of the vine_effect as an imported cpp file

--- build_windows.bat
+++ build_windows.bat
@@ -1,4 +1,4 @@
-@set CommonCLFlags=-O2
+@set CommonCLFlags=-O2 -Zi
 @set MicrosDir=uu.micros
 @set MicrosCLFlags=-I%MicrosDir%/include/ -I%MicrosDir%/libs/glew/include/
 @set MicrosCLFlags=%MicrosDir%/libs/glew/src/glew.c -DGLEW_STATIC opengl32.lib %MicrosCLFlags%
@@ -14,4 +14,4 @@ cl -c -Fo:output\hid.obj -I %HIDAPIDir%\hidapi %HIDAPIDir%\windows\hid.c
 @set CLWarnings=-wd4457 -wd4244 -wd4305 -wd4267 -wd4996 -wd4505 %CLWarnings%
 @set CLWarnings=-wd4800 %CLWarnings%
 @set Defines=-DOS=OS_WINDOWS -DCPU=CPU_X86_64 -DNEIGE_SLOW
-cl %CommonCLFlags% -Fo:output\ -Fe:neige neige.cpp %MicrosCLFlags% %HIDAPIProducts% -nologo -EHsc -FC %Defines% %CLWarnings%
+cl %CommonCLFlags% -Fo:output\ -Fe:neige neige_unit.cpp %MicrosCLFlags% %HIDAPIProducts% -nologo -EHsc -FC %Defines% %CLWarnings%
